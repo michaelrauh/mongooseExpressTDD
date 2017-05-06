8f4b9d20676644c4e64cd11872d96321c01fb0a9
michaelrauh <michaelrauh@ymail.com>
2017-05-06 13:50:21 -0400

    WIP demonstrating writing and reading from mongoose

--- user.test.js
+++ user.test.js
@@ -1,38 +1,50 @@
 var mongoose = require("mongoose");
-const s = require('./user');
+// const s = require('./user');
 var subject;
 
+afterEach(() => {
+  mongoose.connection.close();
+});
+
 test('can insert a user into the database', done => {
-      var uristring =
-        process.env.MONGODB_URI ||
-        'mongodb://localhost/User';
+  var uristring =
+    process.env.MONGODB_URI ||
+    'mongodb://localhost/User';
 
-      var userSchema = new mongoose.Schema({
-        userid: String,
-        answer: String
-      });
+  var userSchema = new mongoose.Schema({
+    userid: String,
+    answer: String
+  });
 
-      var User = mongoose.model('User', userSchema);
+  var User = mongoose.model('User', userSchema);
 
-      function onConnect(done) {
-        var user = {
-          userid: "foo",
-          answer: "bar"
-        }
+  function onConnect(done) {
+    var user = {
+      userid: "foo",
+      answer: "bar"
+    }
 
-        function onSave(done){
+    function onSave(done) {
 
-          function onFound(done){
-            console.log("here")
-            done()
-          }
-          User.findOne({id: "abc123"}, function(err, doc){
-            console.log(err)
-          })
-        }
-
-        new User(user).save(onSave(done))
+      function onFound(done) {
+        console.log("here")
+        done()
       }
-        mongoose.connect(uristring, onConnect(done))
-        mongoose.connection.close()
-      });
+
+      User.findOne({
+        userid: "foo"
+      }, function(err, doc) {
+        if (err) console.log('Error on save!')
+        expect(doc.userid).toEqual("foo")
+        done()
+      })
+    }
+
+    new User(user).save(onSave(done))
+  }
+  mongoose.connect(uristring, {
+    server: {
+      poolSize: 1
+    }
+  }, onConnect(done))
+});
