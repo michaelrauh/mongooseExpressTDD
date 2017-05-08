d94b1b9e8acc5a439b6f2bb2c4ae4a774f48e243
michaelrauh <michaelrauh@ymail.com>
2017-05-07 20:12:55 -0400

    app connects to db on startup and responds to post with 200

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 var profile = require('./profile')
 
 profile.connect()
+
+app.post('/', function (req, res) {
+  res.sendStatus(200)
+});
+
+var server = app.listen(port, function () {
+  console.log('app listening at port %s', port);
+});
+
+module.exports = server;
--- app.test.js
+++ app.test.js
@@ -1,13 +1,29 @@
+var request = require('supertest');
 var sinon = require('sinon')
-var subject;
 var profile;
 var connectStub;
-beforeEach(() => {
+var subject;
+
+beforeAll(() => {
   profile = require('./profile')
   connectStub = sinon.stub(profile, 'connect')
   subject = require('./app');
 });
 
+afterAll(done => {
+  subject.close(done)
+});
+
 test('loading app forms a database connection', () => {
   expect(connectStub.called).toEqual(true)
+})
+
+test('POST to / should result in a 200', done => {
+  request(subject)
+  .post('/')
+    .expect(200)
+    .end(function(err, res) {
+      if (err) throw err;
+      done();
+    });
 })
